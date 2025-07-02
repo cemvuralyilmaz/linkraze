1ca4af9b551bdf973f8ed875dfea0460e716edfa
Cem Vural Yılmaz <yilmazcemvural35@gmail.com>
2025-07-02 23:31:07 +0300

    version1

---
+++ src/main/java/com/url/shortener/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.url.shortener.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+}
